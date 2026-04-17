5ffebb1f407e54e2288e4aacd61e0edbe00d4625
Hari Dharanesh SP <150462561+haridharanesh-official@users.noreply.github.com>
2026-04-17 15:13:53 +0530

    Update main.py

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -1,5 +1,5 @@
-
 import time, json
+
 from modules.sensor import get_data
 from modules.network import is_online
 from modules.mqtt_client import connect, publish
@@ -8,16 +8,33 @@ from modules.sync_manager import sync_cached_data
 from config.config import CHECK_INTERVAL
 
 def main():
+    print("🚀 Edge Gateway Started...", flush=True)
+
     init_db()
+    print("✅ Database Initialized", flush=True)
+
     connect()
+    print("✅ MQTT Connected", flush=True)
+
     while True:
         data = get_data()
+        print("📊 Generated Data:", data, flush=True)
+
         if is_online():
-            if not publish(data):
+            print("🌐 Internet Available", flush=True)
+
+            if publish(data):
+                print("📡 Published to MQTT", flush=True)
+            else:
+                print("⚠️ Publish failed → caching", flush=True)
                 insert_data(json.dumps(data))
         else:
+            print("❌ No Internet → caching", flush=True)
             insert_data(json.dumps(data))
+
         sync_cached_data()
+        print("🔁 Sync check done", flush=True)
+
         time.sleep(CHECK_INTERVAL)
 
 if __name__ == "__main__":
